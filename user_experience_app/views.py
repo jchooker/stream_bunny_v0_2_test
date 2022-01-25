56396693b13dbbb2e0bc1bd081541c36f6269bc4
Matthew Summers <imadethisforcoding@gmail.com>
2022-01-24 20:32:03 -0600

    2nd

--- user_experience_app/views.py
+++ user_experience_app/views.py
@@ -15,21 +15,34 @@ def favorite_movies_main_page(request):
 
 def members_list_page(request):
     user = User.objects.get(id=request.session['user_id'])
-
+    members = User.objects.all()
     context = {
         "name_of_page" : "members_list_page",
         "user" : user,
+        "members" : members
     }
     return render(request,'members_list_page.html',context)
     
-
-def movie_info_discussion_page(request,movie_id):
+def member_profile(request, member_id):
     user = User.objects.get(id=request.session['user_id'])
-    movie = Movie.objects.get(id=movie_id)
+    member = User.objects.get(id=member_id)
+    member_likes = member.liked_by.all()
+    # user_likes = user.liked_by.all()
+    context ={
+        "name_of_page" : "member_profile_page",
+        "member" : member,
+        "member_likes" : member_likes,
+        "user" : user,
+        # "user_liked" : user_likes,
+    }
+    return render(request, 'member_profile.html', context)
+
+def movie_info_discussion_page(request):
+    user = User.objects.get(id=request.session['user_id'])
+
     context = {
         "name_of_page" : "movie_info_discussion_page",
         "user" : user,
-        "movie" : movie,
     }
     return render(request,'movie_info_discussion_page.html',context)
     
@@ -47,16 +60,18 @@ def user_info_page_edit(request):
 
     context = {
         "name_of_page" : "user_info_page_edit",
-        "user" : user
+        "user" : user,
     }
     return render(request,'user_info_page_edit.html',context)
     
 def user_info_page(request):
     user = User.objects.get(id=request.session['user_id'])
+    my_likes = user.liked_by.all()
 
     context = {
         "name_of_page" : "user_info_page",
-        "user" : user
+        "user" : user,
+        "my_likes" : my_likes
     }
     return render(request,'user_info_page.html',context)
     
@@ -86,10 +101,21 @@ def ue_like(request,movie_id):
         # CHECK TO SEE IF THIS IS RIGHT:
         User.liked_by.delete(movie) ## ???????????
 
-
+        User.liked_by
         # Note.objects.create(body=request.POST['new_note'])    
         context = {
             # 'notes' : Note.objects.all(),
         }
         return render(request,"favorite_movies_main_partial.html",context)
     return redirect('/')
+
+def like(request, movie_id):
+    user = User.objects.get(id=request.session["user_id"])
+
+    Movie.objects.create(imdb_id = movie_id)
+
+    movie = Movie.objects.get(id=movie_id)
+    movie.liked_by.add(user)
+
+    return render(request,"user_info_page.html")
+
